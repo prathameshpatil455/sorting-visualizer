29ee87f10e2b9569ffafa4a4dc12760178fde363
prathameshpatil455 <132359689+prathameshpatil455@users.noreply.github.com>
2024-03-03 10:18:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
+
+function App() {
+
+  return (
+    <>
+      <SortingVisualizer/>
+    </>
+  )
+}
+
+export default App
